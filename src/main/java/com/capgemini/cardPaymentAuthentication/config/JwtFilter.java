d853895de0ba4b5f1538957b73eb522684668898
AnnoyingAlchemist <89044097+AnnoyingAlchemist@users.noreply.github.com>
2026-03-01 21:06:45 -0500

    deleted duplicate dependencies in pom.xml

--- src/main/java/com/capgemini/cardPaymentAuthentication/config/JwtFilter.java
+++ src/main/java/com/capgemini/cardPaymentAuthentication/config/JwtFilter.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -27,7 +26,8 @@ public class JwtFilter extends OncePerRequestFilter {
         String token = null;
 
         if(authHeader != null && authHeader.startsWith("Bearer")){
-            token = authHeader.substring(7); //TODO: replace with regex?
+            //replace with regex?
+            token = authHeader.substring(7);
         }
         if(token!=null && SecurityContextHolder.getContext().getAuthentication() == null){
             Claims claims = jwtService.verifySignatureAndExtractClaims(token);
--- src/main/java/com/capgemini/cardPaymentAuthentication/service/JwtService.java
+++ src/main/java/com/capgemini/cardPaymentAuthentication/service/JwtService.java
@@ -18,7 +18,7 @@ public class JwtService {
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*30))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000*60*60))
                 .addClaims(new HashMap<>())
                 .signWith(getSignedKey(), SignatureAlgorithm.HS256)
                 .compact();
